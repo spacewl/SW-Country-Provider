e6a270f035df7baad841a505a3e53370cc4c4fcd
Andrey Tuzov <andriituzov@gmail.com>
2022-02-10 19:40:27 +0200

    Setup publishing library to the Maven central

--- app/src/test/java/com/spacewl/countryprovider/CountryProviderUnitTest.kt
+++ app/src/test/java/com/spacewl/countryprovider/CountryProviderUnitTest.kt
@@ -1,13 +1,21 @@
 package com.spacewl.countryprovider
 
+import android.content.Context
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class CountryProviderUnitTest {
+    @Mock
+    private lateinit var mockContext: Context
+
     @Test
     fun countries_fetching() {
         val list = runBlocking {
-            CountryProvider.fetchCountries()
+            CountryProvider.fetchCountries(context = mockContext)
         }
         assert(list.isNotEmpty())
     }
